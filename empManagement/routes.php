d87a60183827a0b5148cf322b0b3c8cf51f8a74f
Blessen-VS <134699741+Blessen-VS@users.noreply.github.com>
2023-06-20 10:56:47 +0530

    Add files via upload

---
+++ empManagement/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+    '/about' => 'controllers/about.php',
+];
+
+if (array_key_exists($uri, $routes)){
+    require $routes[$uri];
+}
+?>
---
+++ empManagement/views/about.view.php
@@ -0,0 +1,9 @@
+<?php require("partials/headers.php")?> 
+  <?php require("partials/nav.php")?>
+  <?php require("partials/banner.php")?>
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      Welcome to about
+    </div>
+  </main>
+  <?php require("partials/footers.php")?>
