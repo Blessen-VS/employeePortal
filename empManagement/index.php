9763bff5d9a1818d7349452ed88444677d29d2ac
Blessen-VS <134699741+Blessen-VS@users.noreply.github.com>
2023-06-20 11:08:52 +0530

    Delete empManagement directory

--- empManagement/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require("functions.php");
-require("Database.php");
-//require("routes.php");
-
-$db = new Database();
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
-dd($posts);
--- empManagement/views/contact.view.php
+++
@@ -1,9 +0,0 @@
-<?php require("partials/headers.php")?> 
-  <?php require("partials/nav.php")?>
-  <?php require("partials/banner.php")?>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-     Contact
-    </div>
-  </main>
-  <?php require("partials/footers.php")?>
