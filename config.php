85ab679a0a9ff5898691bd1fa5407b5090663449
Blessen-OP <134699741+Blessen-VS@users.noreply.github.com>
2023-06-22 01:39:35 +0530

    dbconfig

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+
+    'database'=> [
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'employeeportal',
+    'charset' => 'utf8mb4'
+    ]
+];
+?>
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require("functions.php");
 require("Database.php");
 //require("routes.php");
 
-$db = new Database();
+$config = require('config.php');
+
+$db = new Database($config['database']);
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 dd($posts);
